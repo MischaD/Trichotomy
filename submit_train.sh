801ca2f73db591e1a62a3803b21a7d8d2ad0681f
Mischa Dombrowski <mischa.dombrowski@gmail.com>
2024-12-17 07:14:09 +0100

    fixed slurm train script

--- submit_train.sh
+++ submit_train.sh
@@ -3,7 +3,6 @@
 #SBATCH --job-name=TrainEDM2
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:h100:4
-#SBATCH --output=/home/hpc/b180dc/b180dc10/slurm/trichotomy/%x_%j.out  # Standard output
 #SBATCH --export=NONE
 unset SLURM_EXPORT_ENV
 export http_proxy=http://proxy.rrze.uni-erlangen.de:80
@@ -17,7 +16,7 @@ cd /home/atuin/b180dc/b180dc10/pycharm/trichotomy
 source activate /home/atuin/b180dc/b180dc10/conda/edm2
 
 
-#/home/atuin/b180dc/b180dc10/data/trichotomy/images.tar
+#tar xf /home/atuin/b180dc/b180dc10/data/trichotomy/images.tar -C $TMPDIR
 
 export N_GPUS=${SLURM_GPUS_ON_NODE:-1}  # Default to 1 if not set
 
@@ -25,36 +24,46 @@ export N_GPUS=${SLURM_GPUS_ON_NODE:-1}  # Default to 1 if not set
 BASE_DIR="/home/atuin/b180dc/b180dc10/pycharm/trichotomy"
 PROGRAM="edm2/train_edm2.py"
 DATA_BASE_DIR="/home/atuin/b180dc/b180dc10/data/trichotomy"
-#PYTHON_PATH="/vol/ideadata/ed52egek/conda/hugg/bin/python"
 OUTDIR_PREFIX="debug-runs"
-PRETRAIN_PATH="$BASE_DIR/importantmodels/edm2-img512-xs-uncond-2147483-0.045.pkl"
 PRESET="edm2-img512-xs"
-BATCH_GPU="128"
-ENCODER_NORM_MODE="cxr8"
-LR="0.001"
+BATCH_GPU="512"
+LR="0.012"
 DECAY="0.9"
 FP16="True"
 SEED="42"
-SNAPSHOT="8Mi"
-STATUS="8Ki"
+SNAPSHOT="64Mi"
+STATUS="64Ki"
 
 
 #FILELIST="$BASE_DIR/datasets/eight_cxr8_train_debug.txt"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_cond";;
 #FILELIST="$BASE_DIR/datasets/eight_cxr8_train_debug.txt"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_cond";;
 # Filelist and cond_mode configurations
 case "$SLURM_ARRAY_TASK_ID" in
-    0) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_uncond";;
-    1) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_cond";;
-    2) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; COND_MODE="mixed"; OUTDIR="${OUTDIR_PREFIX}/cxr8_mixed";;
-    3) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/dataset1_uncond";;
-    4) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/dataset1_cond";;
-    5) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; COND_MODE="mixed"; OUTDIR="${OUTDIR_PREFIX}/dataset1_mixed";;
-    6) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/dataset2_uncond";;
-    7) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/dataset2_cond";;
-    8) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; COND_MODE="mixed"; OUTDIR="${OUTDIR_PREFIX}/dataset2_mixed";;
+    0) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_uncond";;
+    1) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_cond";;
+    2) FILELIST="$BASE_DIR/datasets/eight_cxr8_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="pseudocond"; OUTDIR="${OUTDIR_PREFIX}/cxr8_pseudocond";;
+    3) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/dataset1_uncond";;
+    4) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/dataset1_cond";;
+    5) FILELIST="$BASE_DIR/datasets/eight_mimic_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="pseudocond"; OUTDIR="${OUTDIR_PREFIX}/dataset1_pseudocond";;
+    6) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="uncond"; OUTDIR="${OUTDIR_PREFIX}/dataset2_uncond";;
+    7) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="cond"; OUTDIR="${OUTDIR_PREFIX}/dataset2_cond";;
+    8) FILELIST="$BASE_DIR/datasets/eight_chexpert_train.txt"; ENCODER_NORM_MODE="cxr8"; COND_MODE="pseudocond"; OUTDIR="${OUTDIR_PREFIX}/dataset2_pseudocond";;
     *) echo "Invalid SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"; exit 1;;
 esac
 
+# set pretrain model
+if [[ $COND_MODE = "uncond" ]]
+then 
+    PRETRAIN_PATH="$BASE_DIR/importantmodels/edm2-img512-xs-uncond-2147483-0.045.pkl"
+elif [[ $COND_MODE = "cond" ]]
+then 
+    PRETRAIN_PATH="$BASE_DIR/importantmodels/edm2-img512-xs-2147483-0.135.pkl"
+elif [[ $COND_MODE = "pseudocond" ]]
+then 
+    PRETRAIN_PATH="$BASE_DIR/importantmodels/training-state-0298844.pt"
+else 
+    echo "Invalid Pretrain Path"; exit 1;;
+fi
 
 # Export PYTHONPATH
 export PYTHONPATH=$BASE_DIR
