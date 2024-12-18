85f9b2875b1cbc499038e5ad5f2a41a951c01f59
Mischa Dombrowski <mischa.dombrowski@gmail.com>
2024-12-18 13:47:24 +0100

    reconstruction script

---
+++ src/compute_reconstructs.py
@@ -0,0 +1,169 @@
+import argparse
+import os
+import pandas as pd
+from torch.utils.data import DataLoader
+from beyondfid.log import logger
+from utils import main_setup, hash_dataset_path
+import torch.multiprocessing as mp
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm
+import numpy as np
+import socket
+from src.latent import compute_latent_representation, get_latent_model
+import os
+import numpy as np
+import torch 
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from diffusers import AutoencoderKL
+
+
+def setup(rank, world_size, master_port):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = str(master_port) 
+    torch.distributed.init_process_group("nccl", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+class LatentDataset(Dataset):
+    def __init__(self, file_list, basedir):
+        self.basedir = basedir
+        self.file_list = file_list
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.basedir, self.file_list[idx]) + ".pt"
+        image = torch.load(img_path)
+        return image, idx, self.file_list[idx]
+
+
+def get_latent_model(path=None):
+    # Load the VQ-VAE model
+    if path is None: 
+        path = "stabilityai/stable-diffusion-2"
+    model = AutoencoderKL.from_pretrained(path, subfolder="vae")
+    model = model.to("cuda")
+    model.eval()
+    return model
+
+
+def get_dataloader(file_list, rank, world_size, config):
+    basedir = config.basedir
+    dataset = LatentDataset(file_list, basedir)  # Replaced VideoDataset with ImageDataset
+    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
+    dataloader = DataLoader(dataset, batch_size=config.compute_latent.batch_size, sampler=sampler, num_workers=4, prefetch_factor=1)
+    return dataloader
+
+
+def get_data_from_folder(path):
+    ALLOWED_EXTENSIONS = ('.png', '.jpg', '.jpeg', '.pt')  # Adjusted to image extensions only
+    # Walk through the directory
+    file_list = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(ALLOWED_EXTENSIONS):
+                # Get the relative file path
+                relative_path = os.path.relpath(os.path.join(root, file), path)
+                file_list.append(relative_path)
+    
+    output_filename = hash_dataset_path(path, img_list=file_list)
+    return file_list, os.path.basename(output_filename)
+
+
+def get_data_from_txt(config):
+    image_paths = []
+    fileDescriptor = open(config.filelist, "r")
+    line = True
+    while line:
+        line = fileDescriptor.readline()
+        if line:
+            lineItems = line.split()
+            image_paths.append(lineItems[0])
+    return image_paths, None
+
+
+def get_data(config):
+    data_csv = pd.read_csv(config.filelist)
+    file_list = list(data_csv["FileName"])
+    if config.debug: 
+        file_list = file_list[:1000]
+    output_filename = hash_dataset_path(os.path.dirname(config.filelist), "".join(file_list))
+    return file_list, os.path.basename(output_filename)
+
+
+def process(rank, world_size, file_list, model, config, save_path):
+    setup(rank, world_size, config.master_port)
+
+    dataloader = get_dataloader(file_list, rank, world_size, config)
+    model = model.to(f"cuda:{rank}")
+    model.eval()
+
+    # Only initialize tqdm for rank 0
+    if rank == 0:
+        pbar = tqdm(dataloader, desc=f"Rank {rank}/{world_size} computing latents", bar_format="{l_bar}{bar:30}{r_bar}{bar:-10b}")
+    else:
+        pbar = dataloader  # No progress bar for other ranks
+
+    with torch.no_grad():
+        for latents, indices, paths in pbar:
+            # images will now be a batch of 16 images, and paths will have 16 corresponding paths
+            latents = latents.to(f"cuda:{rank}")
+            images = model.decode(latents)
+
+            # Save each latent for every image in the batch
+            for i, path in enumerate(paths):
+                img_save_path = os.path.join(save_path, path[:-3])
+                os.makedirs(os.path.dirname(img_save_path), exist_ok=True)
+                Image.fromarray(images[i:i+1].permute(0, 2, 3, 1).cpu().numpy(), 'RGB').save(img_save_path)
+
+    cleanup()
+
+
+def main(config):
+    world_size = torch.cuda.device_count()
+    
+    if config.filelist.endswith(".csv"):
+        file_list, _ =  get_data(config)
+    elif config.filelist.endswith(".txt"): 
+        file_list, _ = get_data_from_txt(config)
+    else: 
+        logger.info(f"Aggregating list of images in folder: {config.filelist}")
+        file_list, _ =  get_data_from_folder(config.filelist)
+
+    latents_output_dir = config.get("output_path", os.path.join(os.path.dirname(config.filelist), "Latents"))
+    os.makedirs(latents_output_dir, exist_ok=True)
+    logger.info(f"Saving latent to {latents_output_dir}")
+
+    model = get_latent_model(path=config.compute_latent.model_path)
+    mp.spawn(process, args=(world_size, file_list, model, config, latents_output_dir), nprocs=world_size, join=True)
+
+    print(f"Latents saved to {latents_output_dir}")
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("EXP_PATH", type=str, help="Path to experiment file")
+    parser.add_argument("EXP_NAME", type=str, help="Path to Experiment results")
+    parser.add_argument("--batch_size", type=int, default=16, help="Batch size for DataLoader")
+    parser.add_argument("--basedir", type=str, help="Path to basedir of latent dataset")
+    parser.add_argument("--output_path", type=str, help="Output path. Default is data_dir/<model>_latents")
+    parser.add_argument("--filelist", type=str, help="data dir or csv with paths to data")
+    parser.add_argument('--master_port', type=int, default=12344)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    config = main_setup(args, name=os.path.basename(__file__).rstrip('.py'))
+    config.compute_latent.batch_size = args.batch_size
+    config.debug = config.EXP_NAME.endswith("debug")
+    config.master_port = args.master_port
+    main(config)
