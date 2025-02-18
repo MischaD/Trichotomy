25ad8e6fb806d9aad888a43a95396a6cabbe3fa9
Mischa Dombrowski <mischa.dombrowski@gmail.com>
2025-02-18 16:00:56 +0100

    Eval All Baseline models

---
+++ full_eval_pipeline_baseline_all.sh
@@ -0,0 +1,105 @@
+#!/bin/bash -l
+#SBATCH --time=18:00:00
+#SBATCH --job-name=EvalFullAll
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:h100:1
+#SBATCH --export=NONE
+
+unset SLURM_EXPORT_ENV
+export http_proxy=http://proxy.rrze.uni-erlangen.de:80
+export https_proxy=http://proxy.rrze.uni-erlangen.de:80
+
+#module load python/3.9-anaconda
+module load python
+module load cuda
+cd /home/atuin/b180dc/b180dc10/pycharm/trichotomy
+source activate /home/atuin/b180dc/b180dc10/conda/edm2
+export N_GPUS=${SLURM_GPUS_ON_NODE:-1}  # Default to 1 if not set
+
+
+PROGRAM="generate_syn_train.py"
+BASE_DIR="/home/atuin/b180dc/b180dc10/pycharm/trichotomy"
+
+case "$SLURM_ARRAY_TASK_ID" in
+    0) MODE="EDM-2-AG"; DS="mimic"; ;;
+    1) MODE="EDM-2"; DS="mimic"; ;;
+    2) MODE="EDM-2-AG"; DS="chexpert"; ;;
+    3) MODE="EDM-2"; DS="chexpert"; ;;
+    4) MODE="EDM-2-AG"; DS="cxr8"; ;;
+    5) MODE="EDM-2"; DS="cxr8"; ;;
+    *) echo "Invalid SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"; exit 1;;
+esac
+
+if [ "$MODE" = "EDM-2-AG" ]; then
+    GMODEL_WEIGHTS="${BASE_DIR}/baseline-runs/${DS}_cond/training-state-0008388.pt"
+    GNET="${BASE_DIR}/baseline-runs/${DS}_cond/network-snapshot-0008388-0.100.pkl"
+
+    MODEL_WEIGHTS="${BASE_DIR}/baseline-runs/${DS}_cond/training-state-0083886.pt"
+    NET="${BASE_DIR}/baseline-runs/${DS}_cond/network-snapshot-0083886-0.100.pkl"
+elif [ "$MODE" = "EDM-2" ]; then
+    GMODEL_WEIGHTS="${BASE_DIR}/baseline-runs/${DS}_uncond/training-state-0008388.pt"
+    GNET="${BASE_DIR}/baseline-runs/${DS}_uncond/network-snapshot-0008388-0.100.pkl"
+
+    MODEL_WEIGHTS="${BASE_DIR}/baseline-runs/${DS}_cond/training-state-0083886.pt"
+    NET="${BASE_DIR}/baseline-runs/${DS}_cond/network-snapshot-0083886-0.100.pkl"
+fi
+
+
+SAVE_NAME="${SLURM_ARRAY_TASK_ID}_baselineall_${MODE}_${DS}" # has to be unique
+DATA_BASE_DIR="/home/atuin/b180dc/b180dc10/data/trichotomy_is"
+DATA_BASE_DIR_LS="/home/atuin/b180dc/b180dc10/data/trichotomy_ls"
+
+COND_MODE="cond"; 
+
+
+OUTDIR_PREFIX="${TMPDIR}/eval_${SAVE_NAME}"
+OUTDIR="${OUTDIR_PREFIX}/${SAVE_NAME}" # beyondfid need double folder
+GUIDANCE="1.4"
+FILELIST="${BASE_DIR}/datasets/eight_${DS}_train.txt"
+FILELIST_VAL="${BASE_DIR}/datasets/eight_${DS}_val.txt"
+FILELIST_TEST="${BASE_DIR}/datasets/eight_${DS}_test.txt"
+TRAINING_PATH_CSV="/home/atuin/b180dc/b180dc10/data/trichotomy_is/eight_${DS}.csv"
+
+
+# Export PYTHONPATH
+export PYTHONPATH=$BASE_DIR
+
+# Log the environment for debugging
+echo "==================================="
+echo "Running training with:"
+echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
+echo "FILELIST: $FILELIST"
+echo "OUTDIR: $OUTDIR"
+echo "model weights: $MODEL_WEIGHTS"
+echo "beyondfid data path: $TRAINING_PATH_CSV"
+echo "==================================="
+
+
+python $PROGRAM \
+    --filelist $FILELIST\
+    --mode $MODE\
+    --outdir "${OUTDIR}"\
+    --model_weights $MODEL_WEIGHTS\
+    --gmodel_weights $GMODEL_WEIGHTS\
+    --net $NET\
+    --gnet $GNET\
+    --cond_mode $COND_MODE\
+    --guidance $GUIDANCE\
+    --basedir $DATA_BASE_DIR_LS\
+
+
+beyondfid $TRAINING_PATH_CSV $TRAINING_PATH_CSV $OUTDIR_PREFIX --feature_extractors swav inception --metrics irs fid --output_path "${BASE_DIR}/results" --results_filename "eval_${SAVE_NAME}.json"
+
+python ./chexnet/Main.py train \
+    --data_dir_train $OUTDIR \
+    --data_dir $DATA_BASE_DIR \
+    --train_file $FILELIST \
+    --val_file $FILELIST_VAL \
+    --test_file $FILELIST_TEST \
+    --outfile downstream/snth_checknext_${SAVE_NAME}.txt \
+    --arch DENSE-NET-121 \
+    --pretrained \
+    --epochs 100 \
+    --resize 256 \
+    --crop 224 \
+    --save_path ./downstream/saved_snth_models_${SAVE_NAME}
